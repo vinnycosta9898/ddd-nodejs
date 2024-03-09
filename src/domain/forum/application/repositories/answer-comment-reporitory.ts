41720fb884ebc65a9ce1b2e31c31d708163189c8
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-03-09 19:27:50 -0300

    Create delete answer comment use case and in memory answer comment

--- src/domain/forum/application/repositories/answer-comment-reporitory.ts
+++ src/domain/forum/application/repositories/answer-comment-reporitory.ts
@@ -1,5 +1,7 @@
-import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
-export interface AnswerCommentRepository {
-  create(answer: AnswerComment): Promise<void>
+export interface AnswerCommentsRepository {
+  create(answerComment: AnswerComment): Promise<void>
+  delete(answerComment: AnswerComment): Promise<void>
+  findById(id: string): Promise<AnswerComment | null>
 }
--- test/repositories/in-memory-answer-comment-repository.ts
+++ test/repositories/in-memory-answer-comment-repository.ts
@@ -1,12 +1,29 @@
-import { AnswerCommentRepository } from '@/domain/forum/application/repositories/answer-comment-reporitory'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comment-reporitory'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
-
 export class InMemoryAnswerCommentsRepository
-  implements AnswerCommentRepository
+  implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
